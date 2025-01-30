4e530fdd70bf425eaf72c5acf3c5faaba483143b
Joao-Machado-UAb <1300213@estudante.uab.pt>
2025-01-30 18:10:39 +0000

    Update singleton_db.py

--- singleton_db.py
+++ singleton_db.py
@@ -1,11 +1,6 @@
 # singleton_db.py
 
-DEFAULT_RESUMO = "Resumo de equações de 7º ano: Aqui pode encontrar um resumo de equações de 7º ano."
-
-
 class SingletonDB:
-    # Classe Singleton para administrar o banco de dados
-
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -15,44 +10,20 @@ class SingletonDB:
         return cls._instance
 
     def get_database(self):
-        # Retorna o banco de dados
         return self._instance.db
 
     def create_instance(self, activity_id):
-        """
-        Cria uma entrada no banco de dados se ela não existir
-                Parâmetros:
-            activity_id: ID da atividade a ser criada
-        Retorna:
-            A entrada criada ou já existente
-        """
         if activity_id not in self._instance.db:
             self._instance.db[activity_id] = {
-                "resumo": DEFAULT_RESUMO,
+                "resumo": "Resumo de equações de 7º ano: Aqui pode encontrar um resumo de equações de 7º ano.",
                 "instrucoes": "https://www.matematica.pt/aulas-matematica.php?ano=7",
             }
         return self._instance.db[activity_id]
 
     def access_data(self, activity_id):
-        """
-        Retorna os dados de uma entrada específica
-
-        Parâmetros:
-            activity_id: ID da atividade a ser acessada
-        Retorna:
-            Os dados da entrada, ou None se não existir
-        """
         return self._instance.db.get(activity_id, None)
 
     def execute_operations(self, activity_id, resumo=None, instrucoes=None):
-        """
-        Atualiza os dados de uma entrada existente no banco de dados
-
-        Parâmetros:
-            activity_id: ID da atividade a ser atualizada
-            resumo: Novo resumo (opcional).
-            instrucoes: Novas instruções (opcional).
-        """
         if activity_id not in self._instance.db:
             raise KeyError(
                 f"Activity ID '{activity_id}' não encontrado no banco de dados."
